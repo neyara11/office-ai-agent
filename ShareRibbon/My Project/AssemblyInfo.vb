7b9fc4911fc55366ea6e5abbde847462b672fe24
1 <1>
2026-09-19 21:58:49 +0300

    chore(release): bump version to 2.8.23
    Sync Version.txt, four AssemblyInfo file/informational versions, three VSTO ApplicationVersion fields, and OfficeAgent.vdproj ProductVersion; new ProductCode/PackageCode for a proper MSI upgrade.

--- ShareRibbon/My Project/AssemblyInfo.vb
+++ ShareRibbon/My Project/AssemblyInfo.vb
@@ -29,6 +29,6 @@ Imports System.Runtime.InteropServices
 '
 
 <Assembly: AssemblyVersion("2.0.0.0")>
-<Assembly: AssemblyFileVersion("2.8.22.0")>
+<Assembly: AssemblyFileVersion("2.8.23.0")>
 
-<Assembly: AssemblyInformationalVersion("2.8.22")>
+<Assembly: AssemblyInformationalVersion("2.8.23")>
