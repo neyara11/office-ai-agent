41c9b73331fc241c777a7a3d3bb67cad7267eb30
1 <1>
2026-09-19 22:04:31 +0300

    chore(release): bump version to 2.8.24
    Sync Version.txt, four AssemblyInfo file/informational versions, three VSTO ApplicationVersion fields, and OfficeAgent.vdproj ProductVersion; new ProductCode/PackageCode for a proper MSI upgrade.

--- PowerPointAi/My Project/AssemblyInfo.vb
+++ PowerPointAi/My Project/AssemblyInfo.vb
@@ -36,10 +36,10 @@ Imports System.Security
 ' <Assembly: AssemblyVersion("2.0.0.0")>
 
 <Assembly: AssemblyVersion("2.0.0.0")>
-<Assembly: AssemblyFileVersion("2.8.23.0")>
+<Assembly: AssemblyFileVersion("2.8.24.0")>
 
 
-<Assembly: AssemblyInformationalVersion("2.8.23")>
+<Assembly: AssemblyInformationalVersion("2.8.24")>
 Friend Module DesignTimeConstants
     Public Const RibbonTypeSerializer As String = "Microsoft.VisualStudio.Tools.Office.Ribbon.Serialization.RibbonTypeCodeDomSerializer, Microsoft.VisualStudio.Tools.Office.Designer, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a"
     Public Const RibbonBaseTypeSerializer As String = "System.ComponentModel.Design.Serialization.TypeCodeDomSerializer, System.Design"
--- ShareRibbon/My Project/AssemblyInfo.vb
+++ ShareRibbon/My Project/AssemblyInfo.vb
@@ -29,6 +29,6 @@ Imports System.Runtime.InteropServices
 '
 
 <Assembly: AssemblyVersion("2.0.0.0")>
-<Assembly: AssemblyFileVersion("2.8.23.0")>
+<Assembly: AssemblyFileVersion("2.8.24.0")>
 
-<Assembly: AssemblyInformationalVersion("2.8.23")>
+<Assembly: AssemblyInformationalVersion("2.8.24")>
